dd97015be411d8158008296d5cb15b7740af3a2d
Loong <admin@mc-m.net>
2023-09-24 19:48:59 +0800

    New main class.

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
 # OISOL
 
-*The JavaScript superset designed for OI.*
+![Logo](/meta/logo.svg) 
 
-![Logo](/meta/logo.svg) OISOL
+![Python](https://img.shields.io/badge/Powered_By-Python-brightgreen.svg)
+
+![STD](https://img.shields.io/badge/OISO_STD-O23-brightgreen.svg)
+
+OISOL - *The JavaScript superset designed for OI.*
+
+
+## What is OISOL
+
+The syntax of OISOL is similar to JavaScript, but OISOL integrates many of the operations and operations common to OI. In addition, OISOL is more elegant and supports importing other.ol or.js files.
+
+## How tow use OISOL
+
+The default compiler for OISOL is OCC, and you can convert OISOL files to JavaScript files by using python3 occ.py -o [your file name].
+
+### Staffs / Thanks
+
+- Jankie
---
+++ main/oisol.py
@@ -0,0 +1,34 @@
+class OISOLError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+    
+    def __str__(self):
+        return self.msg
+    
+    def logger(self):
+        pass # Todo: Add log for error.
+
+class version:
+    def __init__(self,num1,num2,num3) -> None:
+        self.num1 = num1
+        self.num2 = num2
+        self.num3 = num3
+    
+    def __len__(self):
+        return len(self.num1) + len(self.num2) + len(self.num3)
+    
+    def __repr__(self):
+        try:
+            print(self.num1 + self.num2 + self.num3)
+        except:
+            raise RuntimeError
+        
+class OISOL:
+    def __init__(self,version,filename) -> None:
+        self.version = version
+        self.filename = filename
+    
+    def getOisolSTD() -> version:
+        ver = version('0','2','3')
+        return ver
+
